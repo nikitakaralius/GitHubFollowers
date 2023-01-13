5b3c42c2f0e10e22ef674fff5a01239ba35c2bb6
nikitakaralyus <nikitakaralyus@gmail.com>
2023-01-13 18:37:24 +0300

    Created VC stubs

--- GitHubFollowers/ViewControllers/SearchVC.swift
+++ GitHubFollowers/ViewControllers/SearchVC.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class SearchVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemIndigo
