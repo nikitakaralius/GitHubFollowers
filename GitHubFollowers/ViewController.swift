37cbb1ecc4dfb7ac4e9d0060590a885990dc2c1f
nikitakaralyus <nikitakaralyus@gmail.com>
2023-01-13 13:34:00 +0300

    Initial commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Nikita Karalius on 13.01.2023.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
